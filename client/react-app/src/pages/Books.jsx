496fc6c1bfd8457bf1019d6699cdc23ab9d89932
nemanjamartic <nemanja.martic@gmail.com>
2025-07-23 23:00:18 +0200

    Fetch Data From MySql

--- client/react-app/src/pages/Books.jsx
+++ client/react-app/src/pages/Books.jsx
@@ -1,8 +1,37 @@
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import axios from "axios";
 
 const Books = () => {
+    const [books, setBooks] = useState([]);
+
+    useEffect(() => {
+        const fetchAllBooks = async () => {
+            try {
+                const result = await axios.get("http://localhost:8000/books");
+                setBooks(result.data);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        fetchAllBooks()
+    }, [])
+
     return (
-        <div>Books</div>
+        <div>
+            <h1>Book</h1>
+            <div className="books">
+                {books.map(book => (
+                    <div className="book" key={book.id}>
+                        {book.cover && <img src={book.cover} alt=""/>}
+                        <h2>{book.title}</h2>
+                        <p>{book.description}</p>
+                        <span>{book.price}</span>
+                    </div>
+                ))}
+            </div>
+        </div>
     )
 }
 
