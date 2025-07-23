721ed5e39b7e806a2abe8bd3a50082bb2391a22a
nemanjamartic <nemanja.martic@gmail.com>
2025-07-23 18:23:12 +0200

    Inotial commit.

---
+++ backend/index.js
@@ -0,0 +1,40 @@
+import express from 'express'
+import mysql from 'mysql'
+
+const app = express()
+
+const db = mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"",
+    database:"nodejs_react"
+})
+
+app.use(express.json())
+
+app.get("/", (req, res) => {
+    res.json("Backend part!")
+})
+
+app.get("/books", (req, res) => {
+    const q = "SELECT * FROM books";
+    db.query(q, (err, data) => {
+        if (err) return res.json(err);
+        return res.json(data);
+    });
+})
+
+app.post("/books", (req, res) => {
+    const now = new Date();
+    const q = "INSERT INTO books (`title`, `description`, `cover`, `created_at`, `updated_at`, `created_by`, `updated_by`) VALUES (?)"
+    const values = ['Fencing book', 'Fencing book description', 'cover.png', now, now, 1, 1]
+
+    db.query(q, [values], (err, data) => {
+        if (err) return res.json(err);
+        return res.json(data);
+    })
+})
+
+app.listen(8800, () => {
+    console.log('Connected to backend!');
+});
---
+++ client/react-app/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Books from './pages/Books'
+import Update from './pages/Update'
+import Add from './pages/Add'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Books/>}/>
+          <Route path="/add" element={<Add/>}/>
+          <Route path="/update" element={<Update/>}/>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
